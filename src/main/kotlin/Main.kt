389d909a355d4c4d2bb4f2c443349ebc8eb1ec32
Ted Osborne <eccentricon@users.noreply.github.com>
2022-08-13 14:49:33 -0400

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+/*
+ * Bounty Board Challenges
+ */
+const val HERO_NAME = "Madrigal"
+
+fun main() {
+    println("The hero announces her presence to the world.")
+
+    println(HERO_NAME)
+    var playerLevel = 4
+    println(playerLevel)
+
+    println("The hero embarks on her journey to locate the enchanted sword.")
+    playerLevel += 1
+    println(playerLevel)
+}
